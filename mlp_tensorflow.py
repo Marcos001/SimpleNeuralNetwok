906e21fb8c83209c5d5c465e7c60d659e0ad2084
marcos <ferreiraviniciussdi@gmail.com>
2019-05-21 00:53:10 -0300

    mlp keras

--- mlp_tensorflow.py
+++ mlp_tensorflow.py
@@ -2,7 +2,7 @@ import tensorflow as tf
 import numpy as np
 
 
-def model(features, labels, mode):
+def MLPTF(features, labels, mode):
 
     # Build a linear model and predict values
     W = tf.get_variable("W", [1], dtype=tf.float64)
@@ -21,43 +21,8 @@ def model(features, labels, mode):
     # appropriate functionality.
     return tf.contrib.learn.ModelFnOps(mode=mode, predictions=y, loss=loss, train_op=train)
 
-def main():
 
-    #carregando os dados
-
-    from data import  loading_data_train
-
-    matrix = loading_data_train()
-
-    train_X = []
-    train_y = []
-    test_X = [0, 1, 3]
-    test_y = [3, 2, 1]
-
-    vetor_x = []
-
-    for i in matrix:
-        lis_tmp = []
-        try:
-            value = float((i[1]))
-            train_y.append(int(i[0]))
-            lis_tmp.append(value)
-            train_X.append(lis_tmp)
-            vetor_x.append((train_y, train_X))
-        except:
-            value = float((i[1]))
-            lis_tmp.append(value)
-            train_X.append(lis_tmp)
-            train_y.append(int(ord(i[0])))
-
-    print('vendo os dados da base > ')
-
-    print('train x = ', len(train_X), ' <> ', train_X)
-    print('train y = ', len(train_y), ' <> ',  train_y)
-
-    print('test x = ', len(test_X), ' <> ',  test_X)
-    print('test y = ', len(test_y), ' <> ',  test_y)
-
+    """
     #treinando a rede
 
     estimator = tf.contrib.learn.Estimator(model_fn=model)
@@ -72,12 +37,4 @@ def main():
 
     # evaluate our model
     print(estimator.evaluate(input_fn=input_fn, steps=10))
-
-
-
-
-if __name__ == '__main__':
-    '''
-    '''
-    main()
-    print('finish run.')
+    """
